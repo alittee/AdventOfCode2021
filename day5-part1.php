500852c80240e1bb672a2bb1f194efaad3af0ae1
Adrian Littee <adrianlittee@gmail.com>
2021-12-08 23:47:52 +1000

    Solve day 5 part 2

--- day5-part1.php
+++ day5-part1.php
@@ -6,7 +6,7 @@ function GetIndex($axis, $const, $value) {
 
 function GetLine($coords, $axis, $const, $v1, $v2) {
 	if ($v1 == $v2) {
-	  $index = GetIndex($axis, $const, $v1);
+	  	$index = GetIndex($axis, $const, $v1);
 		$coords[$index] += 1;
 		return $coords;
 	}
@@ -14,8 +14,9 @@ function GetLine($coords, $axis, $const, $v1, $v2) {
 	$xy1 = ($v1 > $v2) ? $v1: $v2;
 	$xy2 = ($v1 > $v2) ? $v2: $v1;
 	
-	while ($xy1 > $xy2) {
-    $index = GetIndex($axis, $const, $xy2);
+	while ($xy1 >= $xy2) {
+    	$index = GetIndex($axis, $const, $xy2);
+    	//var_dump($index);
 		$coords[$index] += 1;
 		$xy2++;
 	}
@@ -29,6 +30,8 @@ function GetCords($inputs) {
 	foreach($inputs as $input) {
 		$split = explode("->", $input);
 		
+		$some = array();
+		
 		$left = trim($split[0]);
 		$right = trim($split[1]);
 		
@@ -38,24 +41,37 @@ function GetCords($inputs) {
 		$y2 = (int) explode(",", $split[1])[1];
 		
 		if ($x1 == $x2) {
-	    $cords[$]
+	    	$some = GetLine($cords, 'x', $x1, $y1, $y2);
+		} else if ($y1 == $y2) {
+			$some = GetLine($cords, 'y', $y1, $x1, $x2);
 		}
-		if ($y1 == $y2) {
-		  
+		
+		$cords = array_merge($cords, $some);
+	}
+	
+	return $cords;
+}
+
+function GetResults($cords) {
+	$count = 0;
+	foreach ($cords as $cord) {
+		if ($cord >= 2) {
+			$count++;
 		}
 	}
-	var_dump($cords);
+	return $count;
 }
 
 function GetInputs() {
-	//$file_name = "day5-inputs.txt";
-	//$file = fopen($file_name, "r") or die("Unable to open file!");
-	//$inputs = explode("\n", fread($file, filesize($file_name)));
-	//fclose($file);
+	$file_name = "day5-inputs.txt";
+	$file = fopen($file_name, "r") or die("Unable to open file!");
+	$inputs = explode("\n", fread($file, filesize($file_name)));
+	fclose($file);
 	
-	return $inputs = ["0,9 -> 5,9","8,0 -> 0,8","9,4 -> 3,4","2,2 -> 2,1","7,0 -> 7,4","6,4 -> 2,0","0,9 -> 2,9","3,4 -> 1,4","0,0 -> 8,8","5,5 -> 8,2"];
+	return $inputs;
 }
 
 $inputs = GetInputs();
-GetCords($inputs);
+$cords = GetCords($inputs);
+var_dump(GetResults($cords));
 ?>

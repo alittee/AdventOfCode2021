f0af953520a21eab3eb15c51b4c324c914a27866
Adrian Littee <adrianlittee@gmail.com>
2021-12-10 13:36:34 +1000

    Day 7 yew

--- day6-part2.php
+++ day6-part2.php
@@ -1,28 +1,40 @@
 <?php
-function GetFish($school, $days) {
+
+function GetSchool($input) : array
+{
+	$school = array_fill(0, 9, 0);
+	
+	foreach($input as $number) {
+		$school[$number]++;
+	}
+	
+	return $school;
+}
+
+function UpdateSchool($school) : array
+{
+	$growth = array_shift($school);
+	$school[6] += $growth;
+	$school[8] = $growth;
+	
+	return $school;
+}
+
+function GetFish($school, $days) : int
+{
 	for ($i = 0; $i < $days; $i++) {
-		$size = count($school);
-		$growth = 0;
-		
-		foreach($school as $index => $fish) {
-			if ($fish == 0) {
-				$school[$index] = 6;
-				$growth++;
-			} else {
-				$school[$index]--;
-			}
-		}
-		
-		for ($d = 0; $d < $growth; $d++) {
-			$school[$size] = 8;
-			$size++;
-		}
+		$school = UpdateSchool($school);
 	}
 
-	return count($school);
+	return array_sum($school);
 }
-$days = 80;
+
+$days = 256;
 $small = [3,4,3,1,2];
 $input = [5,1,4,1,5,1,1,5,4,4,4,4,5,1,2,2,1,3,4,1,1,5,1,5,2,2,2,2,1,4,2,4,3,3,3,3,1,1,1,4,3,4,3,1,2,1,5,1,1,4,3,3,1,5,3,4,1,1,3,5,2,4,1,5,3,3,5,4,2,2,3,2,1,1,4,1,2,4,4,2,1,4,3,3,4,4,5,3,4,5,1,1,3,2,5,1,5,1,1,5,2,1,1,4,3,2,5,2,1,1,4,1,5,5,3,4,1,5,4,5,3,1,1,1,4,5,3,1,1,1,5,3,3,5,1,4,1,1,3,2,4,1,3,1,4,5,5,1,4,4,4,2,2,5,5,5,5,5,1,2,3,1,1,2,2,2,2,4,4,1,5,4,5,2,1,2,5,4,4,3,2,1,5,1,4,5,1,4,3,4,1,3,1,5,5,3,1,1,5,1,1,1,2,1,2,2,1,4,3,2,4,4,4,3,1,1,1,5,5,5,3,2,5,2,1,1,5,4,1,2,1,1,1,1,1,2,1,1,4,2,1,3,4,2,3,1,2,2,3,3,4,3,5,4,1,3,1,1,1,2,5,2,4,5,2,3,3,2,1,2,1,1,2,5,3,1,5,2,2,5,1,3,3,2,5,1,3,1,1,3,1,1,2,2,2,3,1,1,4,2];
-var_dump(GetFish($input, $days));
+
+$school = GetSchool($input);
+
+var_dump(GetFish($school, $days));
+
 ?>
